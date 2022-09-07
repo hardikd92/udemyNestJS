5eacc995a0a2441786892f0777c8f776bec43422
Hardik Bhatia <bhatia.hardik@yahoo.com>
2022-09-07 19:28:13 +0530

    pending

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -15,10 +15,7 @@ export class messagesRepository{
         const contents = await readFile('messages.json', 'utf8');
         const messages = JSON.parse(contents);
         const id = Math.floor(Math.random() * 999);
-
         messages[id] = { id, content };
-
         await writeFile('messages.json', JSON.stringify(messages));
-
     }
 }
